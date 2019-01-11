7bade41808aa69619ca113cdaec25332907b3ace
Louis Martin <louismartin@fb.com>
2019-01-11 14:06:09 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='worktime_tracker',
+    version='0.1',
+    description='Worktime Tracker',
+    author='Louis Martin',
+    author_email='louisrtm@gmail.com',
+    packages=['worktime_tracker'],
+)
